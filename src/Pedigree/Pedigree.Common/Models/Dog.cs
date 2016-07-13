2eb5a01bff3256c1390f8f1dc38004fa2b6f9a37
John Askew <John Askew>
2016-07-12 21:00:47 -0400

    Initial commit

---
+++ src/Pedigree/Pedigree.Common/Models/Dog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Vouzamo.Interop.Interfaces;
+
+namespace Pedigree.Common.Models
+{
+    public class Dog : IEntity
+    {
+        public Guid Id { get; protected set; }
+        public string Name { get; protected set; }
+        public Sex Sex { get; protected set; }
+
+        protected Dog()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Dog(string name, Sex sex) : this()
+        {
+            Name = name;
+            Sex = sex;
+        }
+    }
+}
---
+++ src/Pedigree/Pedigree.Common/Models/Sex.cs
@@ -0,0 +1,8 @@
+﻿namespace Pedigree.Common.Models
+{
+    public enum Sex
+    {
+        Bitch,
+        Dog
+    }
+}
